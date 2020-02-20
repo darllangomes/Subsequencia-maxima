aca0522b221d4bb048f1447f96adc5de670fffec
Darllam Gomes Ferreira de Lima <darllangomes10@hotmail.com>
2020-02-20 15:25:32 +0000

    Adcionando comentários

--- main.c
+++ main.c
@@ -10,6 +10,14 @@
   int matriz[20][20];
   int i,j;
 
+
+/*nessa função é feita a comparação dos caracteres da matriz que eu nomeei como "sequencia", for igual a "c",eu chamo a função recursivamente para a linha de cima,na coluna anterior da matriz, realizando o "caminho de volta" que o algoritmo necessita(para visualizar melhor esse processo recomendo acessar o link: https://www.cs.usfca.edu/~galles/visualization/DPLCS.html, selecione a opção lcsTable e veja a execução).
+
+Se a opção for "u", eu chamo a função recursivamente para a linha de cima da matriz sequencia
+
+Se não atender a nenhuma das condições anteriores, eu chamo a função recursivamente para a coluna anterior
+da Matriz de sequencia.
+*/
  void imprimir(int k, int l){
        if(k==0 || l==0)
           return;
@@ -24,6 +32,8 @@
                 }
   }
   
+
+
   void lcs(){
   x=strlen(nome1) ;
   y=strlen(nome2) ;
@@ -37,8 +47,8 @@
     	matriz[0][i]=0;
 	}
         
-    //aqui que o preenchimento da matriz acontece
-
+    /*aqui que o preenchimento da matriz acontece segundo as regras -> setar 0 na primeira linha e na primeira coluna. matriz[i-1][j-1] se xi e ji forem iguais e o max(matriz[i][j-1],matriz[i-1][j]),se xi e ji forem diferentes
+  */
     for(i=1;i<=x;i++){
       for(j=1;j<=y;j++){
         if(nome1[i-1]==nome2[j-1]){
